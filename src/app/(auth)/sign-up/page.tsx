b2238a91bb36a9a98d783c0bb2d59a65a09b70c0
Opeyemi Esther Agbaje <damamy96@gmail.com>
2024-01-23 09:50:56 +0100

    Update

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+export default function SignUp() {
+    return(
+        <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
+           <form>
+            <h1 className="text-3xl font-semibold text-white">Sign Up</h1>
+            <div className="space-y-4 mt-5">
+
+            </div>
+           </form>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="p-4 text-center">
-      <Button><h1 className="text-center font-bold text-4xl text-pink-500">Opeyemi</h1></Button>
+    <div className="text-center">
+      <Button><h1 className="p-4 text-center font-bold text-lg text-pink-400">Button</h1></Button>
     </div>
   )
 }
